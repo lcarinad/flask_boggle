baea26b1ee4756e675d6a095cec4eb2a4f594854
Lauren DeLago <123128869+lcarinad@users.noreply.github.com>
2023-09-30 22:23:30 -0400

    validity word check and scoreboard

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session, jsonify
+from flask import Flask, render_template, request, redirect, session, jsonify, flash
 
 
 
@@ -9,17 +9,17 @@ from boggle import Boggle
 app.config['SECRET_KEY'] = 'abc123'
 
 boggle_game = Boggle()
-
+board = boggle_game.make_board()
 @app.route('/')
 def index():
-    new_board = Boggle()
-    board = new_board.make_board()
     session['current_board'] = board
     return render_template('index.html', board = board)
 
-@app.route('/word', methods=["POST"])
+@app.route('/word', methods=["POST", "GET"])
 def accept_word():
     submitted_word = request.json.get("word")
-    print("************************")
-    print(submitted_word)
-    return redirect('/')
+    result =  boggle_game.check_valid_word(session['current_board'], submitted_word)
+ 
+    response = jsonify({"result":result})
+
+    return response
--- static/app.js
+++ static/app.js
@@ -1,11 +1,19 @@
 let $submitBtn = $("#submit-btn");
 let checkWordUrl = "http://127.0.0.1:5000/word";
+let $scoreBoard = $("#score-board");
+let result = 0;
 
 async function checkWord(word) {
   let response = await axios.post(checkWordUrl, {
     word,
   });
-  return response.data;
+  let validityCheck = response.data.result;
+
+  if (validityCheck === "ok") {
+    addToScore(word);
+  } else {
+    alert("That word is not on the board.  Try again");
+  }
 }
 
 $("form").on("submit", function (e) {
@@ -14,3 +22,11 @@ $("form").on("submit", function (e) {
   checkWord(word);
   $("#word-input").val("");
 });
+
+function addToScore(word) {
+  //   console.log(`yay you got a point with ${word}`);
+  let points = word.length;
+  let updatedResult = result + points;
+  result = updatedResult;
+  $scoreBoard.text(result);
+}
